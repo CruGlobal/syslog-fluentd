12cef4c6e30eb9c27c67855e3d6cc8be5ef15e50
Matt Drees <matt.drees@cru.org>
2015-08-05 17:38:11 -0600

    add a script to build, test, and deploy the image to docker hub
    note: if --tty is used from a ruby-spawned process, the terminal output gets messed up.

---
+++ build_test_and_push.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/ruby -w
+
+def run(command, failure_message)
+  puts command
+  system command
+  if ($? != 0)
+    puts failure_message
+    exit 1
+  end
+end
+
+puts 'running build'
+
+run 'main/build.sh', 'build failed'
+
+puts 'building test container'
+run 'test/build.sh', 'building test container failed'
+
+
+puts 'running syslog-splunk'
+
+
+# clean up any failed container from a previous run.
+# Normally docker cleans up after itself when we use --rm,
+# but under some circumstances (like an invalid --link) it doesn't.
+output = `docker rm syslog-splunk-container 2>&1`
+if $?.to_i > 0 && output.index("no such id") == nil
+  puts "unable to remove syslog-splunk-container"
+  puts output
+  exit 1
+end
+
+pid = spawn({'NO_TTY' => 'true'}, 'main/run.sh')
+
+begin
+  sleep 8 # hopefully that's enough...
+
+  puts 'running test'
+  run 'test/run_test_locally.sh', 'running test failed'
+  puts 'test finished successfully'
+
+ensure
+  puts "shutting down syslog-splunk (#{pid})"
+
+  `kill #{pid}`
+
+  Process.wait(pid)
+
+end
+
+git_commit = ENV['GIT_COMMIT'] || `git rev-parse --verify HEAD`.strip
+build_number = ENV['BUILD_NUMBER'] || 0
+
+name = "cruglobal/syslog-splunk:#{git_commit}-#{build_number}"
+run "docker tag cruglobal/syslog-splunk #{name}", 'tag failed'
+
+
+run "docker push #{name}", 'push failed'

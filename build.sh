589c50d6a1f922dcbe1162fb1a3e9b18d5e99d41
Josh Starcher <josh.starcher@gmail.com>
2019-01-15 21:24:27 -0700

    Add $ENVIRONMENT to docker tag.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build \
- --tag 056154071827.dkr.ecr.us-east-1.amazonaws.com/syslog-fluentd:$BUILD_NUMBER \
+ --tag 056154071827.dkr.ecr.us-east-1.amazonaws.com/syslog-fluentd:$ENVIRONMENT-$BUILD_NUMBER \
  main;
 
 
